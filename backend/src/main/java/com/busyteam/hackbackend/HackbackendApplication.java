5de6de227c498e5e7a1b8d38a6e5cf18fe8d07fb
cydercode <cydercode@gmail.com>
2022-11-19 16:56:07 +0100

    Backend service

---
+++ backend/src/main/java/com/busyteam/hackbackend/HackbackendApplication.java
@@ -0,0 +1,13 @@
+package com.busyteam.hackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/busyteam/hackbackend/HackbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busyteam.hackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
