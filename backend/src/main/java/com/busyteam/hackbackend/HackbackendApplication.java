e8f260ecb2956d279c6452765b6f6595e0aa9c4c
cydercode <cydercode@gmail.com>
2022-11-19 19:54:33 +0100

    cors

--- backend/src/main/java/com/busyteam/hackbackend/HackbackendApplication.java
+++ backend/src/main/java/com/busyteam/hackbackend/HackbackendApplication.java
@@ -2,11 +2,24 @@ package com.busyteam.hackbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HackbackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HackbackendApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+      }
+    };
   }
 }
